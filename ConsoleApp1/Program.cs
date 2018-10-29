bffa04848e32f359e0555f0d68ae3cc9db42afb0
thomasbtatum <thomasbtatum@users.noreply.github.com>
2018-10-29 17:58:47 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            List<Rate> rates = File.ReadAllLines(args[0]).Skip(1).Select(v => Rate.FromCSV(v)).ToList();
+            var convertedJson = JsonConvert.SerializeObject(rates, Formatting.Indented);
+            int var = 1;
+        }
+
+    }
+
+    public class Rate
+    {
+
+        public int Id;
+        public int Age;
+        public decimal TenYear;
+        public decimal TwentyYear;
+        public decimal AtAgeSixtyFive;
+        public bool Male;
+        public bool Smoker;
+
+        public static Rate FromCSV(string csvLine)
+        {
+            var values = Regex.Split(csvLine, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+            
+            var rate = new Rate();
+            rate.Id = Convert.ToInt32(values[0]);
+            rate.Age = Convert.ToInt32(values[1]);
+            rate.TenYear = TreatDecimals(values[2]);
+            rate.TwentyYear = TreatDecimals(values[3]);
+            rate.AtAgeSixtyFive = TreatDecimals(values[4]);
+            rate.Male = 'F' == Convert.ToChar(values[5]);
+            rate.Smoker = 'Y' == Convert.ToChar(values[6]);
+            return rate;
+
+        }
+
+        public static Decimal TreatDecimals(string decimalString)
+        {
+            if (decimalString == "N/A")
+                return Decimal.Zero;
+            else
+                return Convert.ToDecimal(decimalString.Replace("\\", "").Replace("\"", ""));
+        }
+
+    }
+}
