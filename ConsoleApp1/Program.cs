9c0a3aeb895c9e07b3fe220e281b53f73a90af20
thomasbtatum <thomasbtatum@users.noreply.github.com>
2018-10-30 16:11:47 +0100

    adds data to dynamodb

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,8 @@
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -11,14 +16,119 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var rates = ReadCSV(args);
+            CreateDynamoDbTable(rates);
+            //LoadJsonDataToTable();
+        }
 
-
-
+        private static List<Rate> ReadCSV(string[] args)
+        {
             List<Rate> rates = File.ReadAllLines(args[0]).Skip(1).Select(v => Rate.FromCSV(v)).ToList();
             var convertedJson = JsonConvert.SerializeObject(rates, Formatting.Indented);
             int var = 1;
+            return rates;
         }
 
+        private static void CreateDynamoDbTable(List<Rate> rates)
+        {
+
+            var tableName = string.Format("Rates{0}", DateTime.Now.ToString("yyyyMMddHHmmss"));
+            //var credentials = 
+
+            //new AmazonS3Client(credentials, RegionEndpoint.USEast1);
+            var client = new AmazonDynamoDBClient(new BasicAWSCredentials("AKIAJV65WRVVJMKNNQUA", "IUtTCQtSJG0CHZsXbbF+L+8EinQJ1XFHgCGW9GWW"), RegionEndpoint.USEast1);
+            
+            Console.WriteLine("Getting list of tables");
+            List<string> currentTables = client.ListTables().TableNames;
+            Console.WriteLine("Number of tables: " + currentTables.Count);
+            if (!currentTables.Contains("blah"))
+            {
+                var request = new CreateTableRequest
+                {
+                    TableName = tableName,
+                    AttributeDefinitions = new List<AttributeDefinition>
+            {
+            new AttributeDefinition
+            {
+                AttributeName = "Id",
+                // "S" = string, "N" = number, and so on.
+                AttributeType = "N"
+            }//,
+            //new AttributeDefinition
+            //{
+            //    AttributeName = "Age",
+            //    // "S" = string, "N" = number, and so on.
+            //    AttributeType = "N"
+            //},
+            //new AttributeDefinition
+            //{
+            //    AttributeName = "TenYear",
+            //    AttributeType = "N"
+            //},
+            //            new AttributeDefinition
+            //{
+            //    AttributeName = "TwentyYear",
+            //    // "S" = string, "N" = number, and so on.
+            //    AttributeType = "N"
+            //},
+            //new AttributeDefinition
+            //{
+            //    AttributeName = "AtAgeSixtyFive",
+            //    // "S" = string, "N" = number, and so on.
+            //    AttributeType = "N"
+            //},
+            //new AttributeDefinition
+            //{
+            //    AttributeName = "Male",
+            //    // "S" = string, "N" = number, and so on.
+            //    AttributeType = "S"
+            //},
+            //new AttributeDefinition
+            //{
+            //    AttributeName = "Smoker",
+            //    // "S" = string, "N" = number, and so on.
+            //    AttributeType = "S"
+            //}
+
+            },
+                    KeySchema = new List<KeySchemaElement>
+            {
+            new KeySchemaElement
+            {
+                AttributeName = "Id",
+                // "HASH" = hash key, "RANGE" = range key.
+                KeyType = "HASH"
+            }
+            },
+                    ProvisionedThroughput = new ProvisionedThroughput
+                    {
+                        ReadCapacityUnits = 10,
+                        WriteCapacityUnits = 5
+                    },
+                };
+
+                var response = client.CreateTable(request);
+
+                Console.WriteLine("Table created with request ID: " +
+                    response.ResponseMetadata.RequestId);
+                int var = 1;
+            }
+
+            var table = Table.LoadTable(client, tableName);
+
+            LoadJsonData(table, rates);
+        }
+
+        private static void LoadJsonData(Table table, List<Rate> Rates)
+        {
+            
+            foreach (Rate rate in Rates)
+            {
+                var item = Document.FromJson(JsonConvert.SerializeObject(rate, Formatting.Indented));
+
+                table.PutItem(item);
+            }
+        }
     }
 
     public class Rate
